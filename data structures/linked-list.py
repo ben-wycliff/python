c1254a7edbd0f2c15b9498343c0cfa57255edd44
ben-wycliff <ben12wycliff@gmail.com>
2022-05-18 23:17:30 +0300

    Initialize linked list class

--- data structures/linked-list.py
+++ data structures/linked-list.py
@@ -7,3 +7,31 @@ class Node:
 
     def __repr__(self):
         return f"<Node data: {self.data}>"
+
+
+class LinkedList:
+    """
+    Singly linked list
+    """
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        """
+        Returns true if linked list is empty
+        """
+        return self.head == None
+
+    def size(self):
+        """
+        Returns the size of the linke list object
+        """
+        current_node = self.head
+        count = 0
+
+        while current_node:
+            count += 1
+            current_node = current_node.next_node
+
+        return count
