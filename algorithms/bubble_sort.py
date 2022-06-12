ab65340a768bf2eefd53e54f30cececb3ac4dc5f
ben-wycliff <ben12wycliff@gmail.com>
2022-06-12 08:42:18 +0300

    Adds bubble sort function

---
+++ algorithms/bubble_sort.py
@@ -0,0 +1,24 @@
+def bubble_sort(list):
+    """
+        Sorts a list of items
+        runs in a timespace of O(n)
+    """
+    includes_swap = False
+    i, j = 0, 1
+
+    while j < len(list):
+        if list[j] > list[i]:
+            container = list[i]
+            list[i] = list[j]
+            list[j] = container
+            includes_swap = True
+        j+=1
+        i+=1
+    return bubble_sort(list) if includes_swap else list
+
+
+
+
+    
+
+   
